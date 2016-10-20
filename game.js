010a86c7d74fe2dab0a45e49f8cc5d7a540b7edf
Kevin <kevin.rodriguez@bracksint.com>
2016-10-20 21:51:18 +0000

    Generate tubes

--- game.js
+++ game.js
@@ -2,14 +2,16 @@ var backgroundGame;
 var flappy;
 var tubes;
 var spaceBar;
+var timer;
 var theGame = {
     preload: function(){
         game.load.image('background', 'img/bg.jpeg');
-        game.load.image('tube', 'img/tubo.png');
         game.load.spritesheet('birds', 'img/pajaro.png', 43, 30);
+        game.load.image('tube', 'img/tube.png');
+        game.forceSingleUpdate = true;
     },
     create: function(){
-        backgroundGame = game.add.tileSprite(0, 0, 400, 600, 'background');
+        backgroundGame = game.add.tileSprite(0, 0, 370, 550, 'background');
         flappy = game.add.sprite(100, 250, 'birds');
         tubes = game.add.group();
         tubes.enableBody = true;
@@ -17,16 +19,23 @@ var theGame = {
         flappy.animations.add('fly', [1, 0, 2], 4, true);
         game.physics.startSystem(Phaser.Physics.ARCADE); // Fisicas
         game.physics.arcade.enable(flappy);
-        flappy.body.collideWorldBounds = true
         flappy.body.gravity.y = 1200;
         flappy.anchor.setTo(0, 0.5);
         spaceBar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceBar.onDown.add(this.jump, this);
+        timer = game.time.events.loop(1500, this.tubesWorld, this);
     },
     
     update: function(){
-        backgroundGame.tilePosition.x -= 1;
-        flappy.animations.play('fly');
+        if (flappy.inWorld == false){
+            //alert("Muerto :(");
+        }else if (flappy.position.y > 480){
+            //alert("Muerto :(");
+        }else{
+            backgroundGame.tilePosition.x -= 1;
+            flappy.animations.play('fly');
+        }
+        
         if (flappy.angle < 20){
             flappy.angle += 1;
         }
@@ -35,5 +44,22 @@ var theGame = {
     jump: function(){
         flappy.body.velocity.y = -350;
         game.add.tween(flappy).to({angle: -20}, 100).start();
+    },
+    
+    tubesWorld: function(){
+        var spaceRange = Math.floor(Math.random()*5) + 1;
+        for (var i = 0; i < 8 ; i++){
+            if (i != spaceRange && i != spaceRange + 1){
+                this.createTube(370, i * 55 + 20);
+            }
+        }
+    },
+    
+    createTube: function(x, y){
+        var tube = tubes.getFirstDead();
+        tube.reset(x, y);
+        tube.body.velocity.x = -180;
+        tube.checkWorldBounds = true;
+        tube.outOfBoundsKill = true;
     }
 };
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(400, 600, Phaser.CANVAS, 'container-game');
+var game = new Phaser.Game(370, 550, Phaser.CANVAS, 'container-game');
 
 game.state.add('menu', Menu);
 game.state.add('theGame', theGame);
