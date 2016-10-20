5b5f1efcc64493ff4725769ce977c50ee60b0d7a
Kevin <kevin.rodriguez@bracksint.com>
2016-10-20 16:04:53 +0000

    Anchor point

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 var game = new Phaser.Game(400, 600, Phaser.CANVAS, 'container-game');
 var backgroundGame;
+var button;
 var mainstate = {
     preload: function(){
         // recursos del juego.
         game.load.image('background', '../img/bg.jpeg');
         game.load.image('bird', '../img/pajaro1.png');
+        game.load.image('btn', '../img/btn.png');
     },
     create: function(){
         // Inicia el juego.
         backgroundGame = game.add.tileSprite(0, 0, 400, 600, 'background');
         game.add.sprite(0, 200, 'bird');
+        button = game.add.sprite(game.width/2, game.height/2, 'btn');
+        button.anchor.setTo(0.5, 0.5);
     },
     
     update: function(){
