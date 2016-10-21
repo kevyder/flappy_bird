38dd5f2fe7a185386f94f02df7e2c9306ffc6106
Kevin <kevin.rodriguez@bracksint.com>
2016-10-21 14:25:38 +0000

    Game Over

---
+++ gameover.js
@@ -0,0 +1,18 @@
+var gameover = {
+    preload: function(){
+        game.stage.backgroundColor = '#FFF';
+        game.load.image('btn', 'img/btn.png');
+    },
+    create: function(){
+         var button = this.add.button(game.width/2, game.height/2, 'btn', this.startGame, this);
+         game.input.enabled = true; // Activa los controles
+         button.anchor.setTo(0.5);
+         var text = game.add.text(game.world.centerX, game.world.centerY - 85, "Game Over");
+         text.anchor.setTo(0.5);
+         text.fontSize = 24;
+         text.fill = '#333';
+    },
+    startGame: function(){
+         this.state.start('theGame');
+    }
+};
